d8af986ab8fc1b5cec0162855892e9f798a7557b
Philip Hardy <pehardy12@gmail.com>
2023-12-10 13:06:30 +0000

    continued moving stuff about the place

---
+++ go/2023/day01/day1.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	utils "advent/utils"
+)
+
+func main() {
+	input := utils.LoadFileAsStringSlice("input.txt")
+
+	fmt.Println(input)
+}
--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,3 @@
-module advent/utils
+module advent
 
 go 1.21.5
